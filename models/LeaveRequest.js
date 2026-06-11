61f3d005ccf3aa7e3af245340ceb942f7dd62cc8
SiddharthBohra5116 <siddharthbohra5116@gmail.com>
2026-06-11 16:46:27 +0530

    Support user/teacher dual fields in LeaveRequest for compatibility

--- models/LeaveRequest.js
+++ models/LeaveRequest.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const { LEAVE_REQUEST_STATUSES } = require('../config/constants');
 
 const leaveRequestSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   startDate: { type: String, required: true },
   endDate: { type: String, required: true },
   reason: { type: String, required: true },
@@ -13,6 +14,16 @@ const leaveRequestSchema = new mongoose.Schema({
   appliedAt: { type: Date, default: Date.now }
 }, { timestamps: true });
 
+leaveRequestSchema.pre('save', function(next) {
+  if (this.teacher && !this.user) {
+    this.user = this.teacher;
+  } else if (this.user && !this.teacher) {
+    this.teacher = this.user;
+  }
+  next();
+});
+
 leaveRequestSchema.index({ user: 1, status: 1 });
+leaveRequestSchema.index({ teacher: 1, status: 1 });
 
 module.exports = mongoose.model('LeaveRequest', leaveRequestSchema);
